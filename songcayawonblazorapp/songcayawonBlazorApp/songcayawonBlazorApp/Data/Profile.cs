ee703c11c026cf6e707a32bfa93ac8d12213276e
danientoy <entoy.daheu21@southernchristiancollege.edu.ph>
2024-02-06 14:22:02 +0800

    songcayawonblazorapp

---
+++ songcayawonblazorapp/songcayawonBlazorApp/songcayawonBlazorApp/Data/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BlazorApp.Data
+{
+    [Serializable]
+    public class Profile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+
+    }
+}
+
